9e64dbfdf2211a5d160ea0d8c003c632655d5fe8
Andrej <modranszky.a@gmail.com>
2023-06-19 12:15:55 +0200

    gsi_create_with_geometry and PIX added

--- matice.c
+++ matice.c
@@ -1,2 +1,33 @@
 #include <stdlib.h>
+#include <unistd.h>
 #include "matice.h"
+
+GSI *gsi_create_empty(void){
+    GSI *ptr = (GSI*)malloc(sizeof(GSI));
+    if(ptr == NULL)
+    {
+        return NULL;
+    }
+
+    ptr->width = 0;
+    ptr->height = 0;
+    ptr->px = NULL;
+
+    return ptr;
+}
+
+GSI *gsi_create_with_geometry(unsigned int m, unsigned int n){
+    GSI *ptr = gsi_create_empty();
+
+    ptr->px = (unsigned char*)malloc(m * n * sizeof(unsigned char));
+    if(ptr->px == NULL)
+    {
+        free(ptr);
+        return NULL;
+    }
+
+    ptr->width = m;
+    ptr->height = n;
+
+    return ptr;
+}
--- ukazka_matice.c
+++ ukazka_matice.c
@@ -1,8 +1,12 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "matice.h"
 
 int main(void)
 {
+    GSI *ptr = gsi_create_with_geometry(5,5);
 
+    free(ptr);
+    free(ptr->px);
     return 0;
 }
