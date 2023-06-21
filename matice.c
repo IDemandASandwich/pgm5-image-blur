65980837e8458cb550b81410fd0fec4ed4499936
Andrej <modranszky.a@gmail.com>
2023-06-21 12:36:39 +0200

    still working on gsi_open

--- matice.c
+++ matice.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 #include "matice.h"
 
@@ -50,7 +51,7 @@ char gsi_save_as_pgm5(GSI *img, char *file_name, char *comment){
         return FILE_OPEN_ERROR;
     }
 
-    snprintf(header, sizeof(header), "P5\n#%s\n%d %d\n255\n", comment, img->width, img->height);
+    snprintf(header, sizeof(header), "P5\n#%s\n%d %d 255\n", comment, img->width, img->height);
 
     write_b = write(fd, header, strlen(header));
     if (write_b < 0) {
@@ -72,46 +73,57 @@ char gsi_save_as_pgm5(GSI *img, char *file_name, char *comment){
 }
 
 GSI *gsi_create_by_pgm5(char *file_name) {
-    int width, height, max_val, ptr, c;
-    char temp[3];
+    GSI* img = gsi_create_empty();
+
+    unsigned int width = 0, height = 0, ptr = 0, c = 0;
+    unsigned char temp[3];
 
     int fd = open(file_name, O_RDONLY);
     if (fd < 0) {
+        gsi_destroy(img);
         return NULL;
     }
 
     ptr = read(fd, temp, 2);
     if(ptr != 2 || temp[0] != 80 || temp[1] != 53){
+        gsi_destroy(img);
+        close(fd);
         return NULL;
     }
 
     if(lseek(fd, 1, SEEK_CUR) == -1){
+        gsi_destroy(img);
+        close(fd);
         return NULL;
     }
 
     while (read(fd, &c, 1) == 1 && c != '\n');
 
-    ptr = read(fd, &width, sizeof(int));
+    ptr = read(fd, temp, sizeof(int));
     if(ptr != sizeof(int)){
+        gsi_destroy(img);
         return NULL;
     }
+
+    img->width = atoi(temp);
+    printf("%d\n", img->width);
 
     if(lseek(fd, 1, SEEK_CUR) == -1){
+        gsi_destroy(img);
+        close(fd);
         return NULL;
     }
 
-    ptr = read(fd, &height, sizeof(int));
+    ptr = read(fd, temp, sizeof(int));
     if(ptr != sizeof(int)){
+        gsi_destroy(img);
         return NULL;
     }
+    
+    img->height = atoi(temp);
+    printf("%d", img->height);
 
-    printf("%d\n",width);
-    printf("%d\n",height);
-
-    GSI* img = gsi_create_empty();
-    img->width = width;
-    img->height = height;
-    img->px = (unsigned char*)malloc(width * height * sizeof(unsigned char));
+    img->px = (unsigned char*)malloc(img->width * img->height * sizeof(unsigned char));
 
     return img;
 }
--- ukazka_matice.c
+++ ukazka_matice.c
@@ -6,6 +6,9 @@ int main(void)
 {
     GSI *img = gsi_create_by_pgm5("pic.pgm");
 
+//    GSI *img = gsi_create_with_geometry_and_color(25, 25, 255);
+//    gsi_save_as_pgm5(img, "pic.pgm", "testing");
+
     gsi_destroy(img);
     return 0;
 }
